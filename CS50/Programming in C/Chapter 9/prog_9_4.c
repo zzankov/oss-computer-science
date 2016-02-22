07d1ea7aaaa524bcd1cd46e9318a6953eecd4c2f
Zahari Zankov <hari.zankov@gmail.com>
2016-02-22 17:54:57 -0500

    Chapter 9 continued...

---
+++ CS50/Programming in C/Chapter 9/prog_9_4.c
@@ -0,0 +1,30 @@
+// Function to determine if two strings are equal
+# include <stdio.h>
+# include <stdbool.h>
+
+bool equalStrings(const char str1[], const char str2[])
+{
+    int   i = 0;
+    bool areEqual;
+
+    while ( str1[i] == str2[i] && str1[i] != '\0' && str2[i] != '\0' )
+        i++;
+    if ( str1[i] == '\0' && str2[i] == '\0' )
+        areEqual = true;
+    else
+        areEqual = false;
+    return areEqual;
+}
+
+int main (void)
+{
+    // prototyping
+    bool equalStrings(const char str1[], const char str2[]);
+    const char stra[] = "string compare test";
+    const char strb[] = "string";
+    printf ("%i\n", equalStrings (stra, strb));
+    printf ("%i\n", equalStrings (stra, stra));
+    printf ("%i\n", equalStrings (strb, "string"));
+
+    return 0;
+}
---
+++ CS50/Programming in C/Chapter 9/prog_9_6.c
@@ -0,0 +1,33 @@
+# include <stdio.h>
+int main(void)
+{
+    int     i;
+    char    line[81];
+
+    // prototype
+    void    readLine (char buffer[]);
+
+    for ( i = 0; i < 3; i++ )
+    {
+         readLine(line);
+         printf ("%s\n\n", line);
+    }
+    return 0;
+}
+
+// function to read a line
+void readLine (char buffer[])
+{
+    char    character;
+    int     i = 0;
+
+    do
+    {
+        character = getchar();
+        buffer[i] = character;
+        i++;
+    } 
+    while ( character != '\n');
+
+    buffer[i - 1] = '\0';
+}
