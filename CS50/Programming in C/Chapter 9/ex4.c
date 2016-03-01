bf9f13fb15be0ee36bd7b12310584e406e2fd571
Zahari Zankov <hari.zankov@gmail.com>
2016-03-01 17:42:30 -0500

    Chapter 9 continued...

--- CS50/Programming in C/Chapter 9/ex4.c
+++ CS50/Programming in C/Chapter 9/ex4.c
@@ -3,7 +3,7 @@
 void substring(const char source[], int start, int count, char result[])
 {
     int i;
-    for ( i = 0; i < count; i++ )
+    for ( i = 0; i < count && source[start + i]; i++ )
         result[i] = source[start + i];
     result[i] = '\0';
 }
@@ -14,6 +14,8 @@ int main (void)
     void substring(const char source[], int start, int count, char result[]);
 
     substring("character", 4, 3, result);
+    printf ("%s\n", result);
+    substring("two words", 4, 20, result);
     printf ("%s\n", result);
     return 0;
 }
